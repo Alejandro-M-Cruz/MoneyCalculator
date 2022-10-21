2afdd7c70307f56ab2bab4fee8148f6f80e5a8b5
aleja <aleja@DESKTOP-GOGHNS2>
2022-10-21 14:37:24 +0100

    Duodécimo commit

--- src/MoneyCalculator/MoneyCalculator.java
+++ src/MoneyCalculator/MoneyCalculator.java
@@ -18,7 +18,7 @@ public class MoneyCalculator {
         DisplayPanel panel = new DisplayPanel(currencies);
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new MoneyCalculatorView(panel, "Currency exchanger");
+                new MoneyCalculatorView(panel, "Currency Exchanger");
             }
         });
     }
