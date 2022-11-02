78238aa843e48b3e0a31bb7b5c4cd2eb61829bc3
aleja <aleja@DESKTOP-GOGHNS2>
2022-11-02 20:21:10 +0000

    Decimonoveno commit

--- src/MoneyCalculator/Main.java
+++ src/MoneyCalculator/Main.java
@@ -11,7 +11,7 @@ import javax.swing.SwingUtilities;
 
 
 
-public class MoneyCalculator {
+public class Main {
 
     public static void main(String[] args) {
         CurrencyLoaderFromFile currencyLoaderFromFile = new CurrencyLoaderFromFile("Currencies.txt");
